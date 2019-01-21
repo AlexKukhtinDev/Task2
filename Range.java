d3ef10dba40a0ddd8cd3be191971644d01a15961
Alex Kukhtin <alex.kukhtin.dev@gmail.com>
2019-01-22 00:59:19 +0300

    Refactor

--- Range.java
+++ Range.java
@@ -1,53 +1,43 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Range {
     public static void main(String[] args) {
-        //input array
-        int arr[] = {1, 2, 3, 5, 8, 9, 10, 13, 14, 15, 16, 18};
-        ArrayList<Integer>[] result = new ArrayList[arr.length];
-        int prev = 0;
-
-        for (int i = 0, k = 0; i < arr.length; i++) {
-            if ((arr[i] - prev) != 1) {
-                k++;
-            }
-
-            if (result[k] == null) {
-                result[k] = new ArrayList<Integer>();
-            }
-
-            result[k].add(arr[i]);
-            prev = arr[i];
-        }
-
-        for (ArrayList<Integer> arrayList : result) {
-            if (arrayList != null) {
-                System.out.print(Arrays.toString(formResult(arrayList)));
-            }
-        }
+        int[] array = {1, 2, 3, 5, 8, 9, 10, 13, 14, 15, 15, 17};
+        range(array.length, array);
     }
 
-    public static Integer[] formResult(ArrayList<Integer> arr) {
-        Integer[] res;
-
-        if (arr == null || arr.toArray().length == 0) {
-            return new Integer[0];
+    private static void range(int length, int[] array) {
+        if (length < 1) {
+            return;
         }
 
-        if (arr.toArray().length > 1) {
-            res = new Integer[2];
+        StringBuilder res = new StringBuilder();
 
-            res[0] = (int) arr.toArray()[0];
-            res[1] = (int) arr.toArray()[arr.toArray().length - 1];
+        int min = array[0];
+        int max = array[0];
 
-            return res;
+        for (int i = 1; i < length; i++) {
+            if ((max + 1) == array[i]) {
+                max++;
+            } else {
+                appendRange(res, min, max);
+                min = array[i];
+                max = array[i];
+            }
+            if (i == length - 1) {
+                appendRange(res, min, max);
+            }
         }
+        System.out.println(res);
+    }
 
-        res = new Integer[1];
-        res[0] = (int) arr.toArray()[0];
-
-
-        return res;
+    private static void appendRange(StringBuilder result, int minR, int maxR) {
+        result.append("[");
+        if (minR == maxR) {
+            result.append(minR);
+        } else {
+            result.append(minR);
+            result.append(" ");
+            result.append(maxR);
+        }
+        result.append("]");
     }
 }
