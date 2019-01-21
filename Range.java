36e1e19c6f1699bedd52242e0ab4488913b41774
Alex Kukhtin <alex.kukhtin.dev@gmail.com>
2019-01-21 23:02:10 +0300

    Correct 3 task

--- Range.java
+++ Range.java
@@ -1,17 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Range {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 5, 8, 9, 10, 13, 14, 15, 16, 18};
-        range(array);
-    }
-
-    private static void range(int[] arr) {
+        //input array
+        int arr[] = {1, 2, 3, 5, 8, 9, 10, 13, 14, 15, 16, 18};
+        ArrayList<Integer>[] result = new ArrayList[arr.length];
         int prev = 0;
-        for (int i = 0; i < arr.length; i++) {
+
+        for (int i = 0, k = 0; i < arr.length; i++) {
             if ((arr[i] - prev) != 1) {
-                System.out.println(" ");
+                k++;
             }
-            System.out.print(arr[i] + " ");
+
+            if (result[k] == null) {
+                result[k] = new ArrayList<Integer>();
+            }
+
+            result[k].add(arr[i]);
             prev = arr[i];
         }
+
+        for (ArrayList<Integer> arrayList : result) {
+            if (arrayList != null) {
+                System.out.print(Arrays.toString(formResult(arrayList)));
+            }
+        }
+    }
+
+    public static Integer[] formResult(ArrayList<Integer> arr) {
+        Integer[] res;
+
+        if (arr == null || arr.toArray().length == 0) {
+            return new Integer[0];
+        }
+
+        if (arr.toArray().length > 1) {
+            res = new Integer[2];
+
+            res[0] = (int) arr.toArray()[0];
+            res[1] = (int) arr.toArray()[arr.toArray().length - 1];
+
+            return res;
+        }
+
+        res = new Integer[1];
+        res[0] = (int) arr.toArray()[0];
+
+        return res;
     }
 }
