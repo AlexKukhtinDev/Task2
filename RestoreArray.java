79d8653283032f8ae3493521df7b54cc022a3efd
Alex Kukhtin <alex.kukhtin.dev@gmail.com>
2019-01-21 21:34:30 +0300

    CorectForm

--- RestoreArray.java
+++ RestoreArray.java
@@ -1,25 +1,17 @@
-import java.sql.Array;
 import java.util.Arrays;
-
 class RestoreArray {
-     public static void main(String[] args) {
-         int [] restor = {1, -1, 3 ,-2 ,7 ,6, -1 , 4,3 ,2};
-         RestoreArray.restoreArray(restor);
-     }
-    private static void restoreArray(int[]restore){
+    public static void main(String[] args) {
+        int[] restor = {1, -1, 3, -2, 7, 6, -1, 4, 3, 2};
+        RestoreArray.restoreArray(restor);
+    }
 
+    private static void restoreArray(int[] restore) {
         System.out.println("This is arrays " + Arrays.toString(restore));
         for (int i = 0; i < restore.length; i++) {
-
             if (0 < i && 0 > restore[i - 1]) {
                 restore[i - 1] = restore[i] - (restore[i] - restore[i - 2]) / 2;
             }
         }
         System.out.print("This is positive number: " + Arrays.toString(restore));
-
-}
-
-
-
-
+    }
 }
---
+++ Rotate.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+public class Rotate {
+    private static int[] rotate(int[] nums, int k) {
+        if(k > nums.length){
+            k = k % nums.length;
+        }
+        int[] result = new int[nums.length];
+        System.arraycopy( nums, k, result, 0, nums.length - k );
+        System.arraycopy( nums, 0, result, nums.length - k , k );
+         nums = result;
+        return nums;
+    }
+    public static void main(String[] args) {
+        int [] arr1 = {5,6,1,2,6};
+        for (int array:Rotate.rotate(arr1,2)) {
+            System.out.print(array + " ");
+        }
+    }
+}
