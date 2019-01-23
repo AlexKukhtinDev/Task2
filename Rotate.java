a202d397acb1fd83a35cfc0e5ecd2e1518858024
Alex Kukhtin <alex.kukhtin.dev@gmail.com>
2019-01-23 15:06:09 +0300

    More correct form

--- Rotate.java
+++ Rotate.java
@@ -1,19 +1,18 @@
 import java.util.Arrays;
 
 public class Rotate {
-     static int[] rotate(int[] nums, int k) {
+    static int[] rotate(int[] nums, int k) {
         if (k > nums.length) {
             k = k % nums.length;
         }
         int[] result = new int[nums.length];
         System.arraycopy(nums, k, result, 0, nums.length - k);
         System.arraycopy(nums, 0, result, nums.length - k, k);
-        result = nums;
         return result;
     }
 
     public static void main(String[] args) {
         int[] array = {5, 6, 1, 2, 6};
-        Arrays.toString(array);
+        System.out.println(Arrays.toString(rotate(array, 2)));
     }
 }
